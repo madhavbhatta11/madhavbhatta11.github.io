98b2ebc5f3079fdf4a0aafc93ccfdd9b1137b014
Madhav Bhatta <madhavbhatta11@gmail.com>
2026-03-18 20:28:55 +0545

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,12 @@
+a=float(input("input your age: "))
+if a>=18:
+    print("can vote")
+else:
+    print("can't vote")
+
+print("Even or odd number")
+n=int(input("Enter a number: "))
+if n%2==0:
+    print("Even")
+else:
+    print("Odd")
