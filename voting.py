f0b3641883bf4130399f99617db645d1fe55a465
Madhav Bhatta <madhavbhatta11@gmail.com>
2026-03-18 21:54:52 +0545

    Update voting.py

--- voting.py
+++ voting.py
@@ -19,3 +19,16 @@ k=0
 while k<=3:
     print(k)
     k+=1
+
+# to print number form 1 to 10
+for l in range(1,11):
+print(l)
+
+#to find sum of number to n
+n = int(input("enter the number: "))
+total = 0
+
+for m in range(1, n + 1):
+    total +=m
+
+print("Final Sum:", total)
