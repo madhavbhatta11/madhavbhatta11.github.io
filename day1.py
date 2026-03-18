05049389fe2b27d949e4d75592743cd8049a6614
Madhav Bhatta <madhavbhatta11@gmail.com>
2026-03-18 19:15:16 +0545

    Refactor day1.py to include additional prints and data structures

--- day1.py
+++ day1.py
@@ -2,3 +2,8 @@ print("helllo buddy")
 a=input("Enter your first name:")
 b=input("enter your last name:")
 print("your full name is", a, b)
+print("hey " 5,6,7, sep="-", end="endng word")
+print("this is next line ")
+list2=["mango" , "orange" , 5 ,5 , 5 ]
+tuple2= { "mango" , 5, 6 , \"nango\" , 5.5555}
+         
