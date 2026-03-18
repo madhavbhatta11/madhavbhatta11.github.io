89b5dc99296cf6e0b4e435e6b9e0c7e7f7d2b8f3
Madhav Bhatta <madhavbhatta11@gmail.com>
2026-03-18 19:30:24 +0545

    Add list and tuple examples in day1topic2.py

---
+++ day1topic2.py
@@ -0,0 +1,9 @@
+#About list and tuples
+cart=["Mango", "banana" ]
+cart.append("apple")
+cart.remove("mango")
+print(cart)
+
+#About tuples , these can't be chabged
+student=( "honey" , 1, "CS")
+print(student)
