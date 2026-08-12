dc33e92029f08f8b5fac60454b0a8b6d80f2b7e3
madhavbhatta11 <madhavbhatta11@gmail.com>
2026-08-12 17:04:13 +0545

    justified text

--- theme.js
+++ theme.js
@@ -6,20 +6,34 @@ const toggle = document.getElementById("theme-toggle");
 const moon = document.getElementById("moon-icon");
 const sun = document.getElementById("sun-icon");
 
+
 // ================= LOAD SAVED THEME =================
 
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme === "dark") {
+
     body.classList.add("dark-mode");
 
-    if (moon) moon.classList.add("hidden");
-    if (sun) sun.classList.remove("hidden");
+    if (moon) {
+        moon.classList.add("hidden");
+    }
+
+    if (sun) {
+        sun.classList.remove("hidden");
+    }
+
 } else {
+
     body.classList.remove("dark-mode");
 
-    if (moon) moon.classList.remove("hidden");
-    if (sun) sun.classList.add("hidden");
+    if (moon) {
+        moon.classList.remove("hidden");
+    }
+
+    if (sun) {
+        sun.classList.add("hidden");
+    }
 }
 
 
@@ -33,17 +47,28 @@ if (toggle) {
 
         const dark = body.classList.contains("dark-mode");
 
+
         if (dark) {
 
-            if (moon) moon.classList.add("hidden");
-            if (sun) sun.classList.remove("hidden");
+            if (moon) {
+                moon.classList.add("hidden");
+            }
+
+            if (sun) {
+                sun.classList.remove("hidden");
+            }
 
             localStorage.setItem("theme", "dark");
 
         } else {
 
-            if (sun) sun.classList.add("hidden");
-            if (moon) moon.classList.remove("hidden");
+            if (sun) {
+                sun.classList.add("hidden");
+            }
+
+            if (moon) {
+                moon.classList.remove("hidden");
+            }
 
             localStorage.setItem("theme", "light");
         }
